4f9576f9a474a3e39d5d5ecc69d8080fef7c85c1
Mehedi-irfan <86709248+Mehedi-irfan@users.noreply.github.com>
2025-01-10 07:12:31 +0000

    type conversion

---
+++ 01_javascipt/type_conversion.js
@@ -0,0 +1,11 @@
+//'33' => if we  convert it number using (Number mehtod) it will show the type number
+//'33abc' => if we  convert it number using (Number mehtod) it will show the type number but answer will be NaN
+// True => if we convert it it's showen 1 if we convert false value that time it's show 0 of type 
+
+let score = '33'
+let scoreNm = Number(score)
+console.log(typeof scoreNm)
+
+// '' => false
+//'irfan' => true
+
