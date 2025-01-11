4db57d7aff852d809448f3ce9b5efb51d75879f6
Mehedi-irfan <86709248+Mehedi-irfan@users.noreply.github.com>
2025-01-11 11:48:03 +0000

    data types summary

---
+++ 01_javascipt/data_types_summery.js
@@ -0,0 +1,11 @@
+//Primitive data types
+//7 types : String, Number, Boolen, Null , Undefind, Symbol, BigInt
+const id = Symbol("123")
+const anotherId = Symbol("123")
+console.table([id, anotherId]);
+console.log(id == anotherId);
+
+
+
+//Reference (Non Premitive)
+//Array, Object, Function
