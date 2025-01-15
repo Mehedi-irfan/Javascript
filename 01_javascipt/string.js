8ec5b1160e3b78a8fc827cb86b812fb03307ec58
Mehedi-irfan <86709248+Mehedi-irfan@users.noreply.github.com>
2025-01-15 14:49:23 +0000

    string summary

---
+++ 01_javascipt/string.js
@@ -0,0 +1,9 @@
+//we can declar string using new keyoward as a object
+const gameName = new String("clash of clan")
+console.log(gameName);
+
+//using charAt we can see which charcter in which position
+const name = 'irfan'
+console.log(name.charAt(2));
+//using index of we also can see which postion have which charcater 
+console.log(name.indexOf('f'));
