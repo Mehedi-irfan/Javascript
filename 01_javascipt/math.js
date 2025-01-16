b83bfe218d998b57a0b682ba410980c4898f8ca0
Mehedi-irfan <86709248+Mehedi-irfan@users.noreply.github.com>
2025-01-16 09:23:12 +0000

    Math method summary

---
+++ 01_javascipt/math.js
@@ -0,0 +1,45 @@
+//in Js Matha is build in function 
+//using Math.PI() we can find pi value
+console.log(Math.PI); // Output: 3.141592653589793
+
+//using Math.round() we can find the nearnest number of integer
+console.log(Math.round(4.7)); // Output: 5
+console.log(Math.round(4.4)); // Output: 4
+
+//using Math.floor() we can find the down of number of integer
+console.log(Math.floor(4.7)); // Output: 4
+
+//using Math.ceil() we can find the upper value of integer
+console.log(Math.ceil(4.2)); // Output: 5
+
+//using Math.trunc(x) method remove the functional part of the number
+console.log(Math.trunc(4.9)); // Output: 4
+console.log(Math.trunc(-4.9)); // Output: -4
+
+//using Math.abs() we can find the absoulate number 
+console.log(Math.abs(-7.5)); // Output: 7.5
+
+//using Math.pow() we can find exponent 
+console.log(Math.pow(2, 3)); // Output: 8
+
+//using Math.sqrt(x) we can find square value of number
+console.log(Math.sqrt(16)); // Output: 4
+
+//using Math.log(x) method we can find logarthmic value
+console.log(Math.log(1)); // Output: 0
+
+//using Math.sin() method we cand find sin value of number
+console.log(Math.sin(Math.PI / 2)); // Output: 1
+
+//using math.cos() method we can find the cos value
+console.log(Math.cos(0)); // Output: 1
+
+//using Math.randoom() method we can find random number
+console.log(Math.random()); // Output: 0.534567 (varies)
+
+//using Math.max(array) method we can find max value from array
+console.log(Math.max(1, 5, 10)); // Output: 10
+
+//using Math.min(array) method we can find min value from array
+console.log(Math.min(1, 5, 10)); // Output: 1
+
