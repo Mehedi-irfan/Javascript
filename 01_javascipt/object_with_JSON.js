7f5949772beea3bf1a4cb0e9eab5b58f13575ab7
Mehedi-irfan <86709248+Mehedi-irfan@users.noreply.github.com>
2025-01-21 14:05:15 +0000

    little bit explan about object with json api

---
+++ 01_javascipt/object_with_JSON.js
@@ -0,0 +1,11 @@
+//object detructure
+const course = {
+    name : "Python",
+    price : 1000,
+    courseInstructor : "Hitesh"
+}
+
+const {courseInstructor: instructor} = course
+console.log(instructor);
+
+//JSON (JavaScript Object Notation)
