dd5b13737674529634caacd0605567c68a556341
Mehedi-irfan <86709248+Mehedi-irfan@users.noreply.github.com>
2025-01-23 12:55:04 +0000

    while or do while loop

---
+++ 01_javascipt/while_do_while.js
@@ -0,0 +1,19 @@
+/**
+ * A while loop repeatedly executes a block of code as long as a specified condition evaluates to true.
+ */
+// while (condition) {
+//     // Code to be executed
+// }
+
+let count = 1;
+while (count <= 10){
+    console.log(`Count is :- ${count}`);
+    count++    
+}
+
+//do while loop
+let score = 1
+do{
+    console.log(`Score is :- ${score}`);
+    score++
+}while(score <= 10)
