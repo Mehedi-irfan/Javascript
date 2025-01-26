4495615743f78741d4578f98b2cd453eb32fe3d2
Mehedi-irfan <86709248+Mehedi-irfan@users.noreply.github.com>
2025-01-26 14:16:03 +0000

    complete BMI Calculator project

--- Projects/BMI Generator/index.js
+++ Projects/BMI Generator/index.js
@@ -5,6 +5,8 @@ form.addEventListener('submit', (e)=> {
     const height = parseInt(document.getElementById('height').value)
     const weight = parseInt(document.getElementById('Weight').value)
     const results = document.querySelector('.results')
+    const p = document.createElement('p')
+
 
     if(height == '' || height < 0 || isNaN(height)){
         results.innerHTML = `Please give a Valid Height`
@@ -13,14 +15,15 @@ form.addEventListener('submit', (e)=> {
     } else{
         const bmi = (weight /((height*height)/10000)).toFixed(2)
         results.innerHTML = `<span>Your BMI is :- ${bmi}</span>`
+        if(bmi <= 18.6){
+            p.innerHTML = `You have under weight BMI`
+            results.appendChild(p)
+        } else if(bmi >=18.6 && bmi <=24.9){
+            p.innerHTML = `You have Normal Range BMI`
+            results.appendChild(p)
+        } else if(bmi >= 24.9){
+            p.innerHTML = `You have Over weight BMI`
+            results.appendChild(p)
+        }
     }
-    
-    if(bmi <= 18.6){
-        results.innerHTML = `<span>You have under weight BMI<span>`
-    } else if(bmi >=18.6 && bmi <=24.9){
-         results.innerHTML = `<span>You have Normal Range BMI<span>`
-    } else if(bmi >= 24.9){
-         results.innerHTML = `<span>You have Over weight BMI<span>`
-    }
-     
 })
