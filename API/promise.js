474cd97c09126f083cc57f3f48e3949c71242c2c
Mehedi-irfan <86709248+Mehedi-irfan@users.noreply.github.com>
2025-02-01 08:27:06 +0000

    uses promise in different away with async await

--- API/promise.js
+++ API/promise.js
@@ -14,7 +14,7 @@ new Promise((resolve, reject)=> {
         resolve({userName : "Mehedi", email : 'mehediirfan99@gamil.com'})
     }, 1000)
 }).then((user)=> {
-    console.log(user);
+    console.table(user);
 })
 
 //using promise with try catch and finaly
@@ -28,7 +28,7 @@ new Promise((resolve, reject)=> {
         }
     },1000)
 }).then((user)=> {
-    console.log(user);
+    console.table(user);
     return user.userName;
 }).then((userName)=> {
     console.log(userName);
@@ -36,4 +36,26 @@ new Promise((resolve, reject)=> {
     console.log((err));
 }).finally(()=> {
     console.log("The promises either resolved or rejected ");
+})
+
+async function promisees (){
+ try{
+    let url = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+    const data = await url.json();
+    console.log(data);
+ }catch{
+    console.log("e :", error);
+ }finally{
+    console.log("don't fetching");
+ }
+}
+promisees()
+
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+.then((response)=> {
+    return response.json()
+}).then((data)=> {
+    console.log(data);
+}).catch((error)=> {
+    console.log(error);
 })
