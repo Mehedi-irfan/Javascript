bf12c0961a1c445a553ff5b02d9716cffadbeb60
Mehedi-irfan <86709248+Mehedi-irfan@users.noreply.github.com>
2025-02-02 04:36:05 +0000

    learning Constructor

--- OOP.js
+++ OOP.js
@@ -9,3 +9,25 @@ const user = {
 }
 console.log(user.username);
 console.log(user.getTheUserValue());
+
+
+//construactor function
+function User(userName, isLogedIn, logInCount){
+    this.username = userName,
+    this.islogedin = isLogedIn,
+    this.logincount = logInCount;
+
+    this.greeting = function(){
+        console.log(`userName :- ${this.username}`);
+        
+    }
+    return this;
+}
+
+//call this constructor using new keyword cz new keyword make new instance this new keyword never overright original function
+const userOne = new User('hasan', 12, true)
+const userTwo = new User('irfan', 502, false)
+console.log(userOne);
+console.log(userTwo);
+
+//when you use new keyword it's make a empty {} object it's called instance
